05d977efda466814fdff34d40af7b7c764321ec3
artem <artem@coderiver.com.ua>
2018-08-23 16:56:12 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <VInput/>
+  </div>
+</template>
+
+<script>
+import VInput from './components/VInput'
+export default {
+  name: 'App',
+  components: {
+    VInput
+  }
+}
+</script>
+
+<style lang="sass">
+#app 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: red
+  margin-top: 60px
+
+</style>
---
+++ src/components/VInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-input">
+    <input type="text" placeholder="Hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VInput'
+}
+</script>
+
+<style lang="sass">
+
+</style>
